b01b1ee888bc3773692b7e64cc0df6282753c4d4
AdrianGzn <152030272+AdrianGzn@users.noreply.github.com>
2023-11-25 13:29:34 -0600

    Add files via upload

---
+++ funciones.js
@@ -0,0 +1,12 @@
+function cambiarColor(elemento) {
+    elemento.style.backgroundColor = "#c29b61"; // Nuevo color al pasar el cursor
+}
+
+function restaurarColor(elemento) {
+    elemento.style.backgroundColor = "#9C640C"; // Color original
+}
+
+let num1=1;
+let num2=4;
+
+alert
